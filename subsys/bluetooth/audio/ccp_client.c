Johnsonyu0926/zephyr

Bluetooth: CCP: Client: Add support for get provider name
Add support for getting the remote bearer provider
name.

Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

subsys/bluetooth/audio/ccp_client.c
@@ -26,6 +26,7 @@
 LOG_MODULE_REGISTER(bt_ccp_client, CONFIG_BT_CCP_CLIENT_LOG_LEVEL);
 
 static sys_slist_t ccp_client_cbs = SYS_SLIST_STATIC_INIT(&ccp_client_cbs);
+static struct bt_tbs_client_cb tbs_client_cbs;
 
 static struct bt_tbs_client_cb tbs_client_cbs;
 
@@ -49,6 +50,31 @@ struct bt_ccp_client {
 };
 
 static struct bt_ccp_client clients[CONFIG_BT_MAX_CONN];
+
+static struct bt_ccp_client_bearer *get_bearer_by_tbs_index(struct bt_ccp_client *client,
+							    uint8_t index)
+{
+	for (size_t i = 0U; i < ARRAY_SIZE(client->bearers); i++) {
+		struct bt_ccp_client_bearer *bearer = &client->bearers[i];
+
+		if (bearer->discovered && bearer->tbs_index == index) {
+			return bearer;
+		}
+	}
+
+	return NULL;
+}
+
+static struct bt_ccp_client *get_client_by_bearer(const struct bt_ccp_client_bearer *bearer)
+{
+	for (size_t i = 0U; i < ARRAY_SIZE(clients); i++) {
+		if (IS_ARRAY_ELEMENT(clients[i].bearers, bearer)) {
+			return &clients[i];
+		}
+	}
+
+	return NULL;
+}
 
 static struct bt_ccp_client *get_client_by_conn(const struct bt_conn *conn)
 {
@@ -246,3 +272,103 @@ int bt_ccp_client_get_bearers(struct bt_ccp_client *client, struct bt_ccp_client
 
 	return 0;
 }
+
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)
+static void tbs_client_read_string_cb(struct bt_conn *conn, int err, uint8_t inst_index,
+				      const char *name)
+{
+	struct bt_ccp_client *client = get_client_by_conn(conn);
+	struct bt_ccp_client_cb *listener, *next;
+	struct bt_ccp_client_bearer *bearer;
+
+	atomic_clear_bit(client->flags, CCP_CLIENT_FLAG_BUSY);
+
+	bearer = get_bearer_by_tbs_index(client, inst_index);
+	if (bearer == NULL) {
+		LOG_DBG("Could not lookup bearer for client %p and index 0x%02X", client,
+			inst_index);
+
+		return;
+	}
+
+	SYS_SLIST_FOR_EACH_CONTAINER_SAFE(&ccp_client_cbs, listener, next, _node) {
+		if (listener->bearer_provider_name != NULL) {
+			listener->bearer_provider_name(bearer, err, name);
+		}
+	}
+}
+
+/**
+ * @brief Validates a bearer and provides a client with ownership of the busy flag
+ *
+ * @param[in] bearer The bearer to validate
+ * @param[out] client A client identified by the @p bearer with the busy flag set if return
+ *                    value is 0.
+ *
+ * @return 0 if the bearer is valid and the @p client has been populated, else an error.
+ */
+static int validate_bearer_and_get_client(const struct bt_ccp_client_bearer *bearer,
+					  struct bt_ccp_client **client)
+{
+	CHECKIF(bearer == NULL) {
+		LOG_DBG("bearer is NULL");
+
+		return -EINVAL;
+	}
+
+	if (!bearer->discovered) {
+		LOG_DBG("bearer %p is not discovered", bearer);
+
+		return -EFAULT;
+	}
+
+	*client = get_client_by_bearer(bearer);
+	if (*client == NULL) {
+		LOG_DBG("Could not identify client from bearer %p", bearer);
+
+		return -EEXIST;
+	}
+
+	if (atomic_test_and_set_bit((*client)->flags, CCP_CLIENT_FLAG_BUSY)) {
+		LOG_DBG("Client %p identified by bearer %p is busy", *client, bearer);
+
+		return -EBUSY;
+	}
+
+	return 0;
+}
+
+int bt_ccp_client_read_bearer_provider_name(struct bt_ccp_client_bearer *bearer)
+{
+	struct bt_ccp_client *client;
+	int err;
+
+	err = validate_bearer_and_get_client(bearer, &client);
+	if (err != 0) {
+		return err;
+	}
+
+	tbs_client_cbs.bearer_provider_name = tbs_client_read_string_cb;
+
+	err = bt_tbs_client_read_bearer_provider_name(client->conn, bearer->tbs_index);
+	if (err != 0) {
+		atomic_clear_bit(client->flags, CCP_CLIENT_FLAG_BUSY);
+
+		/* Return expected return values directly */
+		if (err == -ENOTCONN || err == -EBUSY) {
+			LOG_DBG("bt_tbs_client_read_bearer_provider_name returned %d", err);
+
+			return err;
+		}
+
+		/* Assert if the return value is -EINVAL as that means we are missing a check */
+		__ASSERT(err != -EINVAL, "err shall not be -EINVAL");
+
+		LOG_DBG("Unexpected error from bt_tbs_client_read_bearer_provider_name: %d", err);
+
+		return -ENOEXEC;
+	}
+
+	return 0;
+}
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */

subsys/bluetooth/audio/shell/ccp_client.c
@@ -16,10 +16,17 @@
 #include <zephyr/bluetooth/audio/ccp.h>
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/shell/shell.h>
+#include <zephyr/shell/shell_string_conv.h>
+#include <zephyr/sys/__assert.h>
 
 #include "host/shell/bt.h"
 
 static struct bt_ccp_client *clients[CONFIG_BT_MAX_CONN];
+
+static struct bt_ccp_client *get_client_by_conn(const struct bt_conn *conn)
+{
+	return clients[bt_conn_index(conn)];
+}
 
 static void ccp_client_discover_cb(struct bt_ccp_client *client, int err,
 				   struct bt_ccp_client_bearers *bearers)
@@ -38,14 +45,33 @@ static void ccp_client_discover_cb(struct bt_ccp_client *client, int err,
 	tbs_count = bearers->tbs_count;
 #endif /* CONFIG_BT_TBS_CLIENT_TBS */
 
-	shell_info(ctx_shell, "Discovery completed with %s%u TBS bearers",
+	shell_info(ctx_shell, "Discovery completed for client %p with %s%u TBS bearers", client,
 		   gtbs_bearer != NULL ? "GTBS and " : "", tbs_count);
 }
+
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)
+static void ccp_client_bearer_provider_name_cb(struct bt_ccp_client_bearer *bearer, int err,
+					       const char *name)
+{
+	if (err != 0) {
+		shell_error(ctx_shell, "Failed to read bearer %p name: %d", (void *)bearer, err);
+		return;
+	}
+
+	shell_info(ctx_shell, "Bearer %p name: %s", (void *)bearer, name);
+}
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
+
+static struct bt_ccp_client_cb ccp_client_cbs = {
+	.discover = ccp_client_discover_cb,
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)
+	.bearer_provider_name = ccp_client_bearer_provider_name_cb,
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
+};
 
 static int cmd_ccp_client_discover(const struct shell *sh, size_t argc, char *argv[])
 {
 	static bool cb_registered;
-
 	int err;
 
 	if (default_conn == NULL) {
@@ -58,20 +84,94 @@ static int cmd_ccp_client_discover(const struct shell *sh, size_t argc, char *ar
 	}
 
 	if (!cb_registered) {
-		static struct bt_ccp_client_cb ccp_client_cbs = {
-			.discover = ccp_client_discover_cb,
-		};
-
 		err = bt_ccp_client_register_cb(&ccp_client_cbs);
 		if (err != 0) {
 			shell_error(sh, "Failed to register CCP client cbs (err %d)", err);
 			return -ENOEXEC;
 		}
+
+		cb_registered = true;
 	}
 
 	err = bt_ccp_client_discover(default_conn, &clients[bt_conn_index(default_conn)]);
 	if (err != 0) {
 		shell_error(sh, "Failed to discover GTBS: %d", err);
+
+		return -ENOEXEC;
+	}
+
+	return 0;
+}
+
+static int validate_and_get_index(const struct shell *sh, const char *index_arg)
+{
+	unsigned long index;
+	int err = 0;
+
+	index = shell_strtoul(index_arg, 0, &err);
+	if (err != 0) {
+		shell_error(sh, "Could not parse index: %d", err);
+
+		return -ENOEXEC;
+	}
+
+	if (index >= CONFIG_BT_CCP_CLIENT_BEARER_COUNT) {
+		shell_error(sh, "Invalid index: %lu", index);
+
+		return -ENOEXEC;
+	}
+
+	return (int)index;
+}
+
+static struct bt_ccp_client_bearer *get_bearer_by_index(uint8_t index)
+{
+	struct bt_ccp_client_bearers bearers;
+	struct bt_ccp_client *client;
+	int err;
+
+	client = get_client_by_conn(default_conn);
+	if (client == NULL) {
+		return NULL;
+	}
+
+	err = bt_ccp_client_get_bearers(client, &bearers);
+	__ASSERT_NO_MSG(err == 0);
+
+#if defined(CONFIG_BT_TBS_CLIENT_GTBS)
+	if (index == 0) {
+		return bearers.gtbs_bearer;
+	} else {
+		return bearers.tbs_bearers[index - 1];
+	}
+#else
+	return bearers.tbs_bearers[index];
+#endif /* CONFIG_BT_TBS_CLIENT_GTBS */
+}
+
+static int cmd_ccp_client_read_bearer_name(const struct shell *sh, size_t argc, char *argv[])
+{
+	struct bt_ccp_client_bearer *bearer;
+	unsigned long index = 0;
+	int err;
+
+	if (argc > 1) {
+		index = validate_and_get_index(sh, argv[1]);
+		if (index < 0) {
+			return index;
+		}
+	}
+
+	bearer = get_bearer_by_index(index);
+	if (bearer == NULL) {
+		shell_error(sh, "Failed to get bearer for index %lu", index);
+
+		return -ENOEXEC;
+	}
+
+	err = bt_ccp_client_read_bearer_provider_name(bearer);
+	if (err != 0) {
+		shell_error(sh, "Failed to read bearer[%lu] provider name: %d", index, err);
 
 		return -ENOEXEC;
 	}
@@ -94,6 +194,8 @@ SHELL_STATIC_SUBCMD_SET_CREATE(ccp_client_cmds,
 			       SHELL_CMD_ARG(discover, NULL,
 					     "Discover GTBS and TBS on remote device",
 					     cmd_ccp_client_discover, 1, 0),
+			       SHELL_CMD_ARG(read_bearer_name, NULL, "Get bearer name [index]",
+					     cmd_ccp_client_read_bearer_name, 1, 1),
 			       SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_ARG_REGISTER(ccp_client, &ccp_client_cmds, "Bluetooth CCP client shell commands",

Johnsonyu0926/zrtos

subsys/bluetooth/audio/ccp_client.c
@@ -26,6 +26,7 @@
 LOG_MODULE_REGISTER(bt_ccp_client, CONFIG_BT_CCP_CLIENT_LOG_LEVEL);
 
 static sys_slist_t ccp_client_cbs = SYS_SLIST_STATIC_INIT(&ccp_client_cbs);
+static struct bt_tbs_client_cb tbs_client_cbs;
 
 static struct bt_tbs_client_cb tbs_client_cbs;
 
@@ -49,6 +50,31 @@ struct bt_ccp_client {
 };
 
 static struct bt_ccp_client clients[CONFIG_BT_MAX_CONN];
+
+static struct bt_ccp_client_bearer *get_bearer_by_tbs_index(struct bt_ccp_client *client,
+							    uint8_t index)
+{
+	for (size_t i = 0U; i < ARRAY_SIZE(client->bearers); i++) {
+		struct bt_ccp_client_bearer *bearer = &client->bearers[i];
+
+		if (bearer->discovered && bearer->tbs_index == index) {
+			return bearer;
+		}
+	}
+
+	return NULL;
+}
+
+static struct bt_ccp_client *get_client_by_bearer(const struct bt_ccp_client_bearer *bearer)
+{
+	for (size_t i = 0U; i < ARRAY_SIZE(clients); i++) {
+		if (IS_ARRAY_ELEMENT(clients[i].bearers, bearer)) {
+			return &clients[i];
+		}
+	}
+
+	return NULL;
+}
 
 static struct bt_ccp_client *get_client_by_conn(const struct bt_conn *conn)
 {
@@ -81,6 +107,8 @@ static void disconnected_cb(struct bt_conn *conn, uint8_t reason)
 	if (client->conn == conn) {
 		bt_conn_unref(client->conn);
 		client->conn = NULL;
+
+		memset(client->bearers, 0, sizeof(client->bearers));
 	}
 }
 
@@ -246,3 +274,103 @@ int bt_ccp_client_get_bearers(struct bt_ccp_client *client, struct bt_ccp_client
 
 	return 0;
 }
+
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)
+static void tbs_client_read_bearer_provider_name_cb(struct bt_conn *conn, int err,
+						    uint8_t inst_index, const char *name)
+{
+	struct bt_ccp_client *client = get_client_by_conn(conn);
+	struct bt_ccp_client_cb *listener, *next;
+	struct bt_ccp_client_bearer *bearer;
+
+	atomic_clear_bit(client->flags, CCP_CLIENT_FLAG_BUSY);
+
+	bearer = get_bearer_by_tbs_index(client, inst_index);
+	if (bearer == NULL) {
+		LOG_DBG("Could not lookup bearer for client %p and index 0x%02X", client,
+			inst_index);
+
+		return;
+	}
+
+	SYS_SLIST_FOR_EACH_CONTAINER_SAFE(&ccp_client_cbs, listener, next, _node) {
+		if (listener->bearer_provider_name != NULL) {
+			listener->bearer_provider_name(bearer, err, name);
+		}
+	}
+}
+
+/**
+ * @brief Validates a bearer and provides a client with ownership of the busy flag
+ *
+ * @param[in] bearer The bearer to validate
+ * @param[out] client A client identified by the @p bearer with the busy flag set if return
+ *                    value is 0.
+ *
+ * @return 0 if the bearer is valid and the @p client has been populated, else an error.
+ */
+static int validate_bearer_and_get_client(const struct bt_ccp_client_bearer *bearer,
+					  struct bt_ccp_client **client)
+{
+	CHECKIF(bearer == NULL) {
+		LOG_DBG("bearer is NULL");
+
+		return -EINVAL;
+	}
+
+	*client = get_client_by_bearer(bearer);
+	if (*client == NULL) {
+		LOG_DBG("Could not identify client from bearer %p", bearer);
+
+		return -EEXIST;
+	}
+
+	if (!bearer->discovered) {
+		LOG_DBG("bearer %p is not discovered", bearer);
+
+		return -EFAULT;
+	}
+
+	if (atomic_test_and_set_bit((*client)->flags, CCP_CLIENT_FLAG_BUSY)) {
+		LOG_DBG("Client %p identified by bearer %p is busy", *client, bearer);
+
+		return -EBUSY;
+	}
+
+	return 0;
+}
+
+int bt_ccp_client_read_bearer_provider_name(struct bt_ccp_client_bearer *bearer)
+{
+	struct bt_ccp_client *client;
+	int err;
+
+	err = validate_bearer_and_get_client(bearer, &client);
+	if (err != 0) {
+		return err;
+	}
+
+	tbs_client_cbs.bearer_provider_name = tbs_client_read_bearer_provider_name_cb;
+
+	err = bt_tbs_client_read_bearer_provider_name(client->conn, bearer->tbs_index);
+	if (err != 0) {
+		atomic_clear_bit(client->flags, CCP_CLIENT_FLAG_BUSY);
+
+		/* Return expected return values directly */
+		if (err == -ENOTCONN || err == -EBUSY) {
+			LOG_DBG("bt_tbs_client_read_bearer_provider_name returned %d", err);
+
+			return err;
+		}
+
+		/* Assert if the return value is -EINVAL as that means we are missing a check */
+		__ASSERT(err != -EINVAL, "err shall not be -EINVAL");
+
+		LOG_DBG("Unexpected error from bt_tbs_client_read_bearer_provider_name: %d", err);
+
+		return -ENOEXEC;
+	}
+
+	return 0;
+}
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */

subsys/bluetooth/audio/shell/ccp_client.c
@@ -16,10 +16,17 @@
 #include <zephyr/bluetooth/audio/ccp.h>
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/shell/shell.h>
+#include <zephyr/shell/shell_string_conv.h>
+#include <zephyr/sys/__assert.h>
 
 #include "host/shell/bt.h"
 
 static struct bt_ccp_client *clients[CONFIG_BT_MAX_CONN];
+
+static struct bt_ccp_client *get_client_by_conn(const struct bt_conn *conn)
+{
+	return clients[bt_conn_index(conn)];
+}
 
 static void ccp_client_discover_cb(struct bt_ccp_client *client, int err,
 				   struct bt_ccp_client_bearers *bearers)
@@ -38,14 +45,33 @@ static void ccp_client_discover_cb(struct bt_ccp_client *client, int err,
 	tbs_count = bearers->tbs_count;
 #endif /* CONFIG_BT_TBS_CLIENT_TBS */
 
-	shell_info(ctx_shell, "Discovery completed with %s%u TBS bearers",
+	shell_info(ctx_shell, "Discovery completed for client %p with %s%u TBS bearers", client,
 		   gtbs_bearer != NULL ? "GTBS and " : "", tbs_count);
 }
+
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)
+static void ccp_client_bearer_provider_name_cb(struct bt_ccp_client_bearer *bearer, int err,
+					       const char *name)
+{
+	if (err != 0) {
+		shell_error(ctx_shell, "Failed to read bearer %p name: %d", (void *)bearer, err);
+		return;
+	}
+
+	shell_info(ctx_shell, "Bearer %p name: %s", (void *)bearer, name);
+}
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
+
+static struct bt_ccp_client_cb ccp_client_cbs = {
+	.discover = ccp_client_discover_cb,
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)
+	.bearer_provider_name = ccp_client_bearer_provider_name_cb,
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
+};
 
 static int cmd_ccp_client_discover(const struct shell *sh, size_t argc, char *argv[])
 {
 	static bool cb_registered;
-
 	int err;
 
 	if (default_conn == NULL) {
@@ -58,20 +84,100 @@ static int cmd_ccp_client_discover(const struct shell *sh, size_t argc, char *ar
 	}
 
 	if (!cb_registered) {
-		static struct bt_ccp_client_cb ccp_client_cbs = {
-			.discover = ccp_client_discover_cb,
-		};
-
 		err = bt_ccp_client_register_cb(&ccp_client_cbs);
 		if (err != 0) {
 			shell_error(sh, "Failed to register CCP client cbs (err %d)", err);
 			return -ENOEXEC;
 		}
+
+		cb_registered = true;
 	}
 
 	err = bt_ccp_client_discover(default_conn, &clients[bt_conn_index(default_conn)]);
 	if (err != 0) {
 		shell_error(sh, "Failed to discover GTBS: %d", err);
+
+		return -ENOEXEC;
+	}
+
+	return 0;
+}
+
+static int validate_and_get_index(const struct shell *sh, const char *index_arg)
+{
+	unsigned long index;
+	int err = 0;
+
+	index = shell_strtoul(index_arg, 0, &err);
+	if (err != 0) {
+		shell_error(sh, "Could not parse index: %d", err);
+
+		return -ENOEXEC;
+	}
+
+	if (index >= CONFIG_BT_CCP_CLIENT_BEARER_COUNT) {
+		shell_error(sh, "Invalid index: %lu", index);
+
+		return -ENOEXEC;
+	}
+
+	return (int)index;
+}
+
+static struct bt_ccp_client_bearer *get_bearer_by_index(uint8_t index)
+{
+	struct bt_ccp_client_bearers bearers;
+	struct bt_ccp_client *client;
+	int err;
+
+	client = get_client_by_conn(default_conn);
+	if (client == NULL) {
+		return NULL;
+	}
+
+	err = bt_ccp_client_get_bearers(client, &bearers);
+	__ASSERT_NO_MSG(err == 0);
+
+#if defined(CONFIG_BT_TBS_CLIENT_GTBS) && defined(CONFIG_BT_TBS_CLIENT_TBS)
+	/* If GTBS is enabled then it is at index 0. If the index is not 0, then we decrement it so
+	 * that it can be used as a direct index to the TBS bearers (where index 0 is a TBS inst)
+	 */
+	if (index == 0) {
+		return bearers.gtbs_bearer;
+	}
+	index--;
+#elif defined(CONFIG_BT_TBS_CLIENT_GTBS)
+	return bearers.gtbs_bearer;
+#endif
+
+#if defined(CONFIG_BT_TBS_CLIENT_TBS)
+	return bearers.tbs_bearers[index];
+#endif /* CONFIG_BT_TBS_CLIENT_GTBS */
+}
+
+static int cmd_ccp_client_read_bearer_name(const struct shell *sh, size_t argc, char *argv[])
+{
+	struct bt_ccp_client_bearer *bearer;
+	unsigned long index = 0;
+	int err;
+
+	if (argc > 1) {
+		index = validate_and_get_index(sh, argv[1]);
+		if (index < 0) {
+			return index;
+		}
+	}
+
+	bearer = get_bearer_by_index(index);
+	if (bearer == NULL) {
+		shell_error(sh, "Failed to get bearer for index %lu", index);
+
+		return -ENOEXEC;
+	}
+
+	err = bt_ccp_client_read_bearer_provider_name(bearer);
+	if (err != 0) {
+		shell_error(sh, "Failed to read bearer[%lu] provider name: %d", index, err);
 
 		return -ENOEXEC;
 	}
@@ -94,6 +200,8 @@ SHELL_STATIC_SUBCMD_SET_CREATE(ccp_client_cmds,
 			       SHELL_CMD_ARG(discover, NULL,
 					     "Discover GTBS and TBS on remote device",
 					     cmd_ccp_client_discover, 1, 0),
+			       SHELL_CMD_ARG(read_bearer_name, NULL, "Get bearer name [index]",
+					     cmd_ccp_client_read_bearer_name, 1, 1),
 			       SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_ARG_REGISTER(ccp_client, &ccp_client_cmds, "Bluetooth CCP client shell commands",