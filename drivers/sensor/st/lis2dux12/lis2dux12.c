avisconti/zephyr

drivers/sensor: lis2dux12: add lis2duxs12 support
TBC

The LIS2DUXS12 is a smart, digital, 3-axis linear accelerometer whose
MEMS and ASIC have been expressly designed to combine the lowest current
consumption possible with features such as always-on antialiasing
filtering, a finite state machine (FSM) and machine learning core (MLC)
with adaptive self-configuration (ASC), and an analog hub / Qvar sensing
channel.

(https://www.st.com/en/mems-and-sensors/lis2duxs12.html)

Signed-off-by: Armando Visconti <armando.visconti@st.com>

drivers/sensor/st/lis2dux12/lis2dux12.c
@@ -23,6 +23,10 @@
 #include "st_lis2dux12_api.h"
 #endif
 
+#if DT_HAS_COMPAT_STATUS_OKAY(st_lis2duxs12)
+#include "st_lis2duxs12_api.h"
+#endif
+
 LOG_MODULE_REGISTER(LIS2DUX12, CONFIG_SENSOR_LOG_LEVEL);
 
 #define FOREACH_ODR_ENUM(ODR_VAL)			\
@@ -303,5 +307,9 @@ static const struct sensor_driver_api lis2dux12_driver_api = {
 
 
 #define DT_DRV_COMPAT st_lis2dux12
+DT_INST_FOREACH_STATUS_OKAY_VARGS(LIS2DUX12_DEFINE, DT_DRV_COMPAT)
+#undef DT_DRV_COMPAT
+
+#define DT_DRV_COMPAT st_lis2duxs12
 DT_INST_FOREACH_STATUS_OKAY_VARGS(LIS2DUX12_DEFINE, DT_DRV_COMPAT)
 #undef DT_DRV_COMPAT

Johnsonyu0926/zrtos

drivers/sensor/st/lis2dux12/lis2dux12.c
@@ -23,6 +23,10 @@
 #include "lis2dux12_api.h"
 #endif
 
+#if DT_HAS_COMPAT_STATUS_OKAY(st_lis2duxs12)
+#include "lis2duxs12_api.h"
+#endif
+
 LOG_MODULE_REGISTER(LIS2DUX12, CONFIG_SENSOR_LOG_LEVEL);
 
 #define FOREACH_ODR_ENUM(ODR_VAL)			\
@@ -303,5 +307,9 @@ static const struct sensor_driver_api lis2dux12_driver_api = {
 
 
 #define DT_DRV_COMPAT st_lis2dux12
+DT_INST_FOREACH_STATUS_OKAY_VARGS(LIS2DUX12_DEFINE, DT_DRV_COMPAT)
+#undef DT_DRV_COMPAT
+
+#define DT_DRV_COMPAT st_lis2duxs12
 DT_INST_FOREACH_STATUS_OKAY_VARGS(LIS2DUX12_DEFINE, DT_DRV_COMPAT)
 #undef DT_DRV_COMPAT