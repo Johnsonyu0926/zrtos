Johnsonyu0926/zephyr

ethernet: Switching ETH_NET_DEVICE_<INIT/DT_INSTANCE>... to new ones
Switching to ETH_NET_DEVICE_INSTANCE,
ETH_NET_DEVICE_INSTANCE_FROM_DT_INST and ETH_NET_DEVICE_INSTANCE_MULTI.

Thus removing prio and name parameters on all.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@proton.me>

drivers/ethernet/eth_stm32_hal.c
@@ -1510,9 +1510,9 @@ static struct eth_stm32_hal_dev_data eth0_data = {
 	},
 };
 
-ETH_NET_DEVICE_DT_INST_DEFINE(0, eth_initialize,
+ETH_NET_DEVICE_INSTANCE_FROM_DT_INST(0, eth_initialize,
 		    NULL, &eth0_data, &eth0_config,
-		    CONFIG_ETH_INIT_PRIORITY, &eth_api, ETH_STM32_HAL_MTU);
+		    &eth_api, ETH_STM32_HAL_MTU);
 
 #if defined(CONFIG_PTP_CLOCK_STM32_HAL)
 

Johnsonyu0926/zrtos

drivers/ethernet/eth_stm32_hal.c
@@ -1308,9 +1308,9 @@ static struct eth_stm32_hal_dev_data eth0_data = {
 	},
 };
 
-ETH_NET_DEVICE_DT_INST_DEFINE(0, eth_initialize,
+ETH_NET_DEVICE_INSTANCE_FROM_DT_INST(0, eth_initialize,
 		    NULL, &eth0_data, &eth0_config,
-		    CONFIG_ETH_INIT_PRIORITY, &eth_api, NET_ETH_MTU);
+		    &eth_api, NET_ETH_MTU);
 
 #if defined(CONFIG_PTP_CLOCK_STM32_HAL)
 